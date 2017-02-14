d5b20a8ab0080bfb75f0b717eb8df883cd6faa21
cchambe12 <cchamberlain@g.harvard.edu>
2017-02-14 16:39:12 -0500

    updated soil script, made adjustments to data sheet, started thinking about analyzing

--- analyses/scripts/soil.R
+++ analyses/scripts/soil.R
@@ -1,6 +1,9 @@
 ## 8 February 2017 - Cat
 ## Formulating a more functional datasheet for experiment
 
+library(dplyr)
+library(tidyr)
+
 # Clear workspace
 rm(list=ls()) # remove everything currently held in the R memory
 options(stringsAsFactors=FALSE)
@@ -19,3 +22,13 @@ d$individ<-substr(d$NEW, 9, 10)
 tx<-as.data.frame(table(d$TX))
 site<-as.data.frame(table(d$site))
 species<-as.data.frame(table(d$species))
+
+# Make a new dataframe
+df<-as.data.frame(table(d$species,d$TX, d$site))%>%
+  rename(species=Var1)%>%
+  rename(tx=Var2)%>%
+  rename(site=Var3)
+sub<- apply(df, 1, function(row) all(row !=0 ))
+df<-df[sub,]
+
+write.csv(df, "~/Documents/git/freezingexperiment/analyses/output/species.tx.site.csv", row.names=FALSE)
