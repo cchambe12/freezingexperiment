26a89dba50c8eae3ca564626818c8f8f3168bac6
cchambe12 <cchamberlain@g.harvard.edu>
2017-03-27 16:44:02 -0400

    updated data, double freeze, drought significance

---
+++ analyses/scripts/Drought Conditions.R
@@ -0,0 +1,35 @@
+## Cat - 27 March 2017
+# Look at soil moisture for Freezing Experiment
+# Aim: to see if drought was significant enough to include in analysis
+
+# Clear workspace
+rm(list=ls()) # remove everything currently held in the R memory
+options(stringsAsFactors=FALSE)
+graphics.off()
+
+# Load libraries
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(arm)
+library(data.table)
+# Set Working Directory
+setwd("~/Documents/git/freezingexperiment/analyses")
+d<-read.csv("input/soil.moisture.csv",header=TRUE)
+
+# clean up dataframe
+soil <- d %>%
+  dplyr::select(ID, percent, TX)
+soil<-na.omit(soil)
+soil$mean<-ave(soil$percent, soil$ID)
+moisture<- soil %>%
+  dplyr::select(-percent)
+moisture<-moisture[!duplicated(moisture), ]
+moisture<-arrange(moisture, ID)
+  
+tx<- moisture %>%
+  dplyr::select(-ID)%>%
+  group_by(TX)%>%
+  summarise_each(funs(mean))
+                 
+write.csv(tx, "output/Drought.csv", row.names= FALSE)
